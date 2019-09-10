a7e971224622629a0b654dd22bcf920d9f252923
@Deepu <deepueg@users.noreply.github.com>
2019-09-09 22:37:52 -0700

    Update CollectorApiImpl.js

--- js/CollectorApiImpl.js
+++ js/CollectorApiImpl.js
@@ -1,6 +1,8 @@
 import { CollectorApi } from 'collector-api'
+import {TrustedcollectorService, PaymentEvent, LoginEvent } from 'trustedcollector';
 
 CollectorApi.requests().registerPostEventRequestHandler((requestData) => {
+    this.trustedcollectorService.pushEvent("frontline.login", loginEventData);
     //TODO: Write your implementation and resolve or reject the promise
     //return Promise.resolve(result) || Promise.reject(err)
 })
