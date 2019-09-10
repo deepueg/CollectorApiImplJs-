870714557056db631daf512e07d50037eefc98ed
Deepu Ganapathiyadan <DGanapathiyadan@walmartlabs.com>
2019-09-09 22:35:07 -0700

    initial commit

---
+++ js/CollectorApiImpl.js
@@ -0,0 +1,10 @@
+import { CollectorApi } from 'collector-api'
+
+CollectorApi.requests().registerPostEventRequestHandler((requestData) => {
+    //TODO: Write your implementation and resolve or reject the promise
+    //return Promise.resolve(result) || Promise.reject(err)
+})
+CollectorApi.requests().registerAddItemRequestHandler((requestData) => {
+    //TODO: Write your implementation and resolve or reject the promise
+    //return Promise.resolve(result) || Promise.reject(err)
+})
